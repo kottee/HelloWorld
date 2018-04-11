3fc6ace5eb0c81d63b548c612a9e7283c2b7798e
kottee <kotteeswaranviit@gmail.com>
2018-04-11 13:15:03 +0530

    Update HelloworldRouteServiceProvider.php

--- src/Providers/HelloworldRouteServiceProvider.php
+++ src/Providers/HelloworldRouteServiceProvider.php
@@ -5,12 +5,14 @@ namespace Helloworld\Providers;
  
 use Plenty\Plugin\RouteServiceProvider;
 use Plenty\Plugin\Routing\Router;
+use Plenty\Plugin\Log\Loggable;
  
 class HelloworldRouteServiceProvider extends RouteServiceProvider
 {
-   
+   use Loggable;
     public function map(Router $router)
     {
+     $this->getLogger(__METHOD__)->error('Novalnet::CallbackMailNotSend', 'rouetest');
         $router->get('hello','Helloworld\Controllers\ContentController@sayHello');
     }
 }
