2e06d81f7539d189c7a8b656cafe4df5b62cf495
kottee <kotteeswaranviit@gmail.com>
2018-04-11 13:10:42 +0530

    Update HelloworldServiceProvider.php

--- src/Providers/HelloworldServiceProvider.php
+++ src/Providers/HelloworldServiceProvider.php
@@ -4,13 +4,15 @@ namespace Helloworld\Providers;
  
  
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Log\Loggable;
  
 class HelloworldServiceProvider extends ServiceProvider
 {
- 
+ use Loggable;
  
      public function register()
         {
+      $this->getLogger(__METHOD__)->error('Novalnet::CallbackMailNotSend', 'test');
             $this->getApplication()->register(HelloworldRouteServiceProvider::class);
         }
 }
