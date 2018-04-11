43675d4830b3892c196095a33427db307944091a
kottee <kotteeswaranviit@gmail.com>
2018-04-11 13:07:33 +0530

    Update ContentController.php

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -5,11 +5,14 @@ namespace Helloworld\Controllers;
  
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\Log\Loggable;
  
 class ContentController extends Controller
 {
+ use Loggable;
         public function sayHello(Twig $twig)
     {
+         $this->getLogger(__METHOD__)->error('Novalnet::CallbackMailNotSend', 'testdata');
         return $twig->render('Helloworld::content.hello');
     
     }
